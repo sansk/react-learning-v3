62b29565cb914c39ee69a4c08d8f6d786e55db36
sansk <iamsangyk@gmail.com>
2023-05-27 13:23:42 +0530

    Multiple Inputs update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import { DisplayClear } from "./components/1-display-clear-useState/DisplayClear";
 // import ToggleChallenge from "./components/2-toggle-challenge/ToggleChallenge";
 // import { LoginLogout } from './components/3-login-logout-challenge/LoginLogout';
-import { ControlledInput } from './components/4-controlled-Input-challenge/ControlledInput';
+// import { ControlledInput } from './components/4-controlled-Input-challenge/ControlledInput';
+import { MultipleInputs } from "./components/5-multiple-inputs/MultipleInputs";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <DisplayClear /> */}
       {/* <ToggleChallenge /> */}
       {/* <LoginLogout /> */}
-      <ControlledInput />
+      {/* <ControlledInput /> */}
+      <MultipleInputs />
     </div>
   );
 }
---
+++ src/components/5-multiple-inputs/MultipleInputs.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+export const MultipleInputs = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <h4>Multiple Inputs</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="email"
+            className="form-input"
+            id="email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+          <input
+            type="password"
+            className="form-input"
+            id="password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn btn-block" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
