4277166a167b7aca2de197a5d3323c5a2e4d7440
sansk <iamsangyk@gmail.com>
2023-05-27 15:06:45 +0530

    FormData API for uncontrolled Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 // import ToggleChallenge from "./components/2-toggle-challenge/ToggleChallenge";
 // import { LoginLogout } from './components/3-login-logout-challenge/LoginLogout';
 // import { ControlledInput } from './components/4-controlled-Input-challenge/ControlledInput';
-import { OtherInputs } from "./components/6-other-inputs/OtherInputs";
+// import { OtherInputs } from "./components/6-other-inputs/OtherInputs";
+import { UncontrolledInputs } from "./components/7-formdataAPI-uncontrolledInputs/UncontrolledInputs";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <LoginLogout /> */}
       {/* <ControlledInput /> */}
       {/* <MultipleInputs /> */}
-      <OtherInputs />
+      {/* <OtherInputs /> */}
+      <UncontrolledInputs />
     </div>
   );
 }
